0f387c3a1459ccad8e23668204c775f6ebb3df5b
Liz Lam <lizlam.217@gmail.com>
2020-08-04 00:52:05 -0700

    initial add

---
+++ src/routes/index.svelte
@@ -0,0 +1,16 @@
+<style>
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 4em;
+    }
+  }
+</style>
+
+<svelte:head>
+  <title>Hanny the Penguin</title>
+</svelte:head>
+
+<h1>Hanny the Penguin</h1>
+<h3>Which came first? The penguin or the egg?</h3>
+
+<p>Hanny the penguin. What came first? The penguin or the egg?</p>
