c11bc296d9dd0ce0db580ab35b309bd8b056ef29
Liz Lam <lizlam.217@gmail.com>
2020-09-02 00:42:39 -0700

    Redirect if accessing protected route without login.

--- src/routes/downloads.svelte
+++ src/routes/downloads.svelte
@@ -1,5 +1,14 @@
 <script>
+  import { onMount } from "svelte";
   import { fade } from "svelte/transition";
+
+  onMount(() => {
+    const user = netlifyIdentity.currentUser();
+    console.log(user);
+    if (user === null) {
+      window.location.replace("https://hanny-the-penguin.netlify.app");
+    }
+  });
 </script>
 
 <svelte:head>
@@ -8,9 +17,15 @@
 
 <main in:fade>
   <h1>Downloads</h1>
-
-  <p>PDF downloads of Hanny the Penguin.</p>
-  <a href="/pdfs/which-came-first.pdf">
-    Which Came First, the Penguin or the Egg?
-  </a>
+  <ul>
+    <p>PDF downloads of Hanny the Penguin.</p>
+    <li>
+      <a href="/pdfs/which-came-first.pdf">
+        Which Came First, the Penguin or the Egg?
+      </a>
+    </li>
+    <li>
+      <a href="/pdfs/do-you-speak-butterfly.pdf">Do You Speak Butterfly?</a>
+    </li>
+  </ul>
 </main>
