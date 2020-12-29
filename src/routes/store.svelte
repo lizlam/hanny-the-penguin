cc01b5e0bad6521fe64d670fbf106c3ca399a8f4
grepliz <grepliz@pop-os.localdomain>
2020-12-28 23:19:09 -0800

    Add gumroad product link instead of individual items.

--- src/routes/store.svelte
+++ src/routes/store.svelte
@@ -14,26 +14,10 @@
 
 <main in:fade>
   <h1>Support</h1>
-  <p>Support my work by purchasing a PDF bundle or T-shirt.</p>
+  <p>Support my work by <a href="https://gumroad.com/elizabethrich">purchasing a PDF bundle or T-shirt</a>.</p>
 
   <p>Each PDF bundle includes Kindle friendly PDF <br />
      and a Zine Edition PDF ready to <a href="https://www.wikihow.com/Make-a-Zine">print and fold</a>.
   </p>
   <p>The books are always free to read online.</p>
-
-  <div class="gumroad-product-embed" data-gumroad-product-id="ofPlO">
-    <a href="https://gumroad.com/l/ofPlO">
-      Which Came First, the Penguin or the Egg?
-    </a>
-  </div>
-  <br />
-  <div class="gumroad-product-embed" data-gumroad-product-id="vrrGa">
-    <a href="https://gumroad.com/l/vrrGa">Do You Speak Butterfly?</a>
-  </div>
-  <hr />
-  <div class="gumroad-product-embed" data-gumroad-product-id="YTCRq"><a href="https://gumroad.com/l/YTCRq">Wally's Tail - Adult T-shirt</a></div> 
-  
-  <div class="gumroad-product-embed" data-gumroad-product-id="ztClT"><a href="https://gumroad.com/l/ztClT">Wally the Whale - Kids T-shirt</a></div>
-
-
 </main>
